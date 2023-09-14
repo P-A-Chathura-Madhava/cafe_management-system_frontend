95b919db99cbafb98e4e901f9e0425689f49edab
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-09-14 14:37:13 +0530

    apply forgot password feature

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -16,4 +16,11 @@ export class UserService {
         headers:new HttpHeaders().set('Content-Type', 'application/json')
       })
   }
+
+  forgotPassword(data:any) {
+    return this.httpClient.post(this.url+
+      "/user/forgotPassword", data, {
+        headers:new HttpHeaders().set('Content-Type', 'application/json')
+      })
+  }
 }
