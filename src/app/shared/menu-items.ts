1b39426aa5059edb876b2ccaaf8282322ef65a13
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-09-17 12:19:29 +0530

    apply manage user features and API integration

--- src/app/shared/menu-items.ts
+++ src/app/shared/menu-items.ts
@@ -14,7 +14,8 @@ const MENUITEMS = [
     {state: 'category', name: 'Manage Category', type: 'link', icon: 'category', role: 'admin'},
     {state: 'product', name: 'Manage Product', type: 'link', icon: 'inventory_2', role: 'admin'},
     {state: 'order', name: 'Manage Order', type: 'link', icon: 'shopping_cart', role: ''},
-    {state: 'bill', name: 'View Bill', type: 'link', icon: 'backup_table', role: ''}
+    {state: 'bill', name: 'View Bill', type: 'link', icon: 'backup_table', role: ''},
+    {state: 'user', name: 'Manage User', type: 'link', icon: 'people', role: 'admin'}
 ]
 
 @Injectable()
