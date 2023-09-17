0c137213067ce67173509e606bf225f1d5725638
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-09-17 11:12:47 +0530

    apply view bill page and implement API integration

--- src/app/services/bill.service.ts
+++ src/app/services/bill.service.ts
@@ -12,18 +12,24 @@ export class BillService {
   constructor(private httpClient: HttpClient) {}
 
   generateReport(data: any) {
-    return this.httpClient.post(this.url + "/bill/generateReport", data, {
-      headers: new HttpHeaders().set('Content-Type', "application/json")
+    return this.httpClient.post(this.url + '/bill/generateReport', data, {
+      headers: new HttpHeaders().set('Content-Type', 'application/json'),
     });
   }
 
   getPdf(data: any): Observable<Blob> {
-    return this.httpClient.post(this.url + "/bill/getPdf", data, {
+    return this.httpClient.post(this.url + '/bill/getPdf', data, {
       responseType: 'blob',
     });
   }
 
   getBills() {
-    return this.httpClient.get(this.url + "/bill/getBills");
+    return this.httpClient.get(this.url + '/bill/getBills');
+  }
+
+  delete(id: any) {
+    return this.httpClient.post(this.url + '/bill/delete/' + id, {
+      headers: new HttpHeaders().set('Content-Type', 'application/json'),
+    });
   }
 }
